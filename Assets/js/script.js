006f74442da66afe682a7c5ed6c99dea76c5a83b
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-09-27 18:45:23 -0700

    Create generatePassword function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -13,3 +13,34 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function generatePassword() {
+  var passwordCriteria = {
+    length: 0,
+    lowercase: false,
+    uppercase: false,
+    numeric: false,
+    special: false
+  }
+  
+  passwordCriteria.length = getPasswordLength();
+
+
+}
+
+function getPasswordLength() {
+  var msg = "Please enter password length\nMinimum 8 "
+  window.prompt()
+  
+}
+
+// 1. User clicks button, calls function writePassword()
+// 2. writePassword calls function generatePassword()
+// 3. generatePassword calls function getPasswordCriteria()
+// 4. prompt user for length
+// 5. prompt user for lowercase, uppercase, numeric, special
+// 6. call createPassword()
+// 7. add one random character from each type to password
+// 8. fill in remaining chars randomized from selected criteria
+// 9. return completed password
+// 10. write password to document using queryselector
+// 
