5fd9c80f56ef7b3a52717010c82a019a315e29ce
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-09-27 20:15:26 -0700

    Add data validation for password length prompt

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,6 +4,9 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  if (password === null) {
+    return;
+  }
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -23,14 +26,40 @@ function generatePassword() {
   }
   
   passwordCriteria.length = getPasswordLength();
-
+  if (passwordCriteria.length === null) {
+    return null;
+  }
 
 }
 
 function getPasswordLength() {
-  var msg = "Please enter password length\nMinimum 8 characters, maximum 128 characters";
-  var response = window.prompt(msg);
-  
+  const minLength = 8;
+  const maxLength = 128
+  var msgPrompt = "Welcome to the Password Generator\n\n";
+  msgPrompt = msgPrompt + "Please enter the desired password length,\n";
+  msgPrompt = msgPrompt + "from 8 to 128 characters for maximum security.";
+  var msgTooSmall = "Your entry is too small, please try again.";
+  var msgTooLarge = "Your entry is too large, please try again.";
+  var msgNaN = "Your entry is not a number, please try again.";
+  var response = "";
+
+  var entryIsValid = false;
+  while (entryIsValid === false) {
+    response = window.prompt(msgPrompt);
+    if (response === null) {
+      return null;
+    } else if (isNaN(response)) {
+      window.alert(msgNaN);
+    } else if (response >= maxLength) {
+      window.alert(msgTooLarge);
+    } else if (response <= minLength) {
+      window.alert(msgTooSmall);
+    } else {
+      entryIsValid = true;
+    }
+  }
+  console.log(response);
+
   
 }
 
